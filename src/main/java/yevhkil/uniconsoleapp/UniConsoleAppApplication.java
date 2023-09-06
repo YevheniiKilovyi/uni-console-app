8b4e92904b35f3a6894b52fd44300afc707c5b8d
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-09-06 17:38:20 +0300

    feat(common): set up project

---
+++ src/main/java/yevhkil/uniconsoleapp/UniConsoleAppApplication.java
@@ -0,0 +1,13 @@
+package yevhkil.uniconsoleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniConsoleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniConsoleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yevhkil/uniconsoleapp/UniConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package yevhkil.uniconsoleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniConsoleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
