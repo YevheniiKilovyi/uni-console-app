6cb78b91f1d523614234995c02aae89eda2d76ce
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-09-06 18:50:33 +0300

    feature(app): implemented main controller for entrying the application and updated application.properties file

--- src/main/java/yevhkil/uniconsoleapp/UniConsoleAppApplication.java
+++ src/main/java/yevhkil/uniconsoleapp/UniConsoleAppApplication.java
@@ -1,13 +1,42 @@
 package yevhkil.uniconsoleapp;
 
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import yevhkil.uniconsoleapp.command.CommandInvoker;
 
 @SpringBootApplication
 public class UniConsoleAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UniConsoleAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(UniConsoleAppApplication.class, args);
+
+        if (args.length > 0) {
+            System.out.println("Invalid usage. Use the interactive mode to enter commands.");
+            return;
+        }
+
+        CommandInvoker commandInvoker = context.getBean(CommandInvoker.class);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the University Console Application!");
+        System.out.println("Type 'exit' to quit the application.");
+
+        while (true) {
+            System.out.print("Enter a command: ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            commandInvoker.executeCommand(userInput);
+        }
+
+        scanner.close();
+        System.out.println("Thank you for using the University Console Application. Goodbye!");
     }
 
 }
