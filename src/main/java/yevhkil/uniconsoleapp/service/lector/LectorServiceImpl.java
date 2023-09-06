dde7134b8ffff8b00da0370682314c024c6dc753
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-09-06 18:18:03 +0300

    feat(services): added DepartmentService, LectorService and their implementations

---
+++ src/main/java/yevhkil/uniconsoleapp/service/lector/LectorServiceImpl.java
@@ -0,0 +1,25 @@
+package yevhkil.uniconsoleapp.service.lector;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import yevhkil.uniconsoleapp.dto.response.lector.LectorResponseDto;
+import yevhkil.uniconsoleapp.mapper.lector.LectorMapper;
+import yevhkil.uniconsoleapp.model.Lector;
+import yevhkil.uniconsoleapp.repository.lector.LectorRepository;
+
+@RequiredArgsConstructor
+@Service
+public class LectorServiceImpl implements LectorService {
+    private final LectorRepository lectorRepository;
+    private final LectorMapper lectorMapper;
+
+    @Override
+    public List<LectorResponseDto> findLectorsByTemplate(String template) {
+        List<Lector> lectorsByTemplate = lectorRepository.findLectorsByTemplate(template);
+        return lectorsByTemplate.stream()
+                .map(lectorMapper::toDto)
+                .toList();
+    }
+
+}
